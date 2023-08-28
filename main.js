b3f8c16fd5d99ee95ec6173efa499cc2240a6c84
sultanarebaka <117830026+sultanarebaka@users.noreply.github.com>
2023-08-28 13:33:27 +0600

    Update main.js

--- main.js
+++ main.js
@@ -32,3 +32,5 @@ onClick={this.handleChange.bind(this,index)}>{elem.props.title}</li>
         <Panel title="second">This is the second panel</Panel>
         <Panel title="third">This is the third panel</Panel>
       
+
+React.render(<App/>,document.querySelector('#app'));
