acee526758816e6dcb56d5eb64547005070b524c
sultanarebaka <117830026+sultanarebaka@users.noreply.github.com>
2023-09-01 09:06:36 +0600

    Update main.js

--- main.js
+++ main.js
@@ -79,12 +79,15 @@ React.render(<App/>,document.querySelector('#app'));
 
 onClick={this.handleChange.bind(this,index)}>{elem.props.title};
 
-
-
 const App = React.createClass({
   render(){
     return (
       <Tabs selected={1}>
-
-return <li className={style} key={index} onClick={this.handleChange.bind(this,index)}>{elem.props.title}</li>
-        })}
+        <Panel title="first">This is the first panel</Panel>
+        <Panel title="second">This is the second panel</Panel>
+        <Panel title="third">This is the third panel</Panel>
+      </Tabs>
+    )
+  }
+})
+React.render(<App/>,document.querySelector('#app'));
